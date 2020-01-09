fdd7a1c3138adf14da2530bf13e9a129efc839cc
heellish <55760228+heellish@users.noreply.github.com>
2020-01-09 13:49:03 -0300

    Add files via upload

---
+++ login e cadastro java com telas/anderson_JavaAulas/src/aula14exercicios/ModeloEstudante.java
@@ -0,0 +1,37 @@
+package aula14exercicios;
+public class ModeloEstudante {
+    String nome; String matrícula; String turma; String turno;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatrícula() {
+        return matrícula;
+    }
+
+    public void setMatrícula(String matrícula) {
+        this.matrícula = matrícula;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+    
+}
---
+++ login e cadastro java com telas/anderson_JavaAulas/src/aula14exercicios/ModeloNotas.java
@@ -0,0 +1,40 @@
+package aula14exercicios;
+public class ModeloNotas {
+    String primeiraNota; 
+    String segundaNota;
+    String média;
+    String situação;
+
+    public String getPrimeiraNota() {
+        return primeiraNota;
+    }
+
+    public void setPrimeiraNota(String primeiraNota) {
+        this.primeiraNota = primeiraNota;
+    }
+
+    public String getSegundaNota() {
+        return segundaNota;
+    }
+
+    public void setSegundaNota(String segundaNota) {
+        this.segundaNota = segundaNota;
+    }
+
+    public String getMédia() {
+        return média;
+    }
+
+    public void setMédia(String média) {
+        this.média = média;
+    }
+
+    public String getSituação() {
+        return situação;
+    }
+
+    public void setSituação(String situação) {
+        this.situação = situação;
+    }
+    
+}
